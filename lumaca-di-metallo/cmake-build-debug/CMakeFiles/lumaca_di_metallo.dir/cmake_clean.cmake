8f1c5a576b5563ec9d023e0c4acd2fe09e0ec70b
Cappetti99 <56259803+Cappetti99@users.noreply.github.com>
2019-10-07 15:49:21 +0200

    Add files via upload

---
+++ lumaca-di-metallo/cmake-build-debug/CMakeFiles/lumaca_di_metallo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lumaca_di_metallo.dir/main.cpp.o"
+  "lumaca_di_metallo.pdb"
+  "lumaca_di_metallo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lumaca_di_metallo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lumaca-di-metallo/cmake-build-debug/CMakeFiles/lumaca_di_metallo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lumaca_di_metallo.
+# This may be replaced when dependencies are built.
